d3d6da2cf18a94c2c238bfa0e12a00c56e54be70
Diogo <diogo.a.v@hotmail.com>
2021-10-28 12:02:26 +0100

    Finalizar layout e responsividade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { DataService } from './data.service';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,10 +7,26 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
+  bg: string = 'https://source.unsplash.com/random/1920x1080';
+
+  dateTime?: Date;
 
   constructor(public router: Router) {}
 
-  ngOnInit() {}
+
+  ngOnInit() {
+    this.dateTime = new Date();
+  }
+
+  async changeBg() {
+   const result = await fetch('https://source.unsplash.com/random/1920x1080', {
+    method:'HEAD'
+    })
+
+    
+
+    this.bg = result.url;
+  }
 }
